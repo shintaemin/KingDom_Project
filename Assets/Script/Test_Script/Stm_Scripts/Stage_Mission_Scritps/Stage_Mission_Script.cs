84cc5cf156b7b87b0f7664479b13923c2efcc847
shintaemin <xoalsdl125@gmail.com>
2026-03-19 18:47:24 +0900

    feat : create - StageMissionScript
    미션 스크립트 생성만
    설계후 내용채우기
    현재단계에서 큰 틀은 있지만 세부구현을 어떻게 연결할지 고민..

---
+++ Assets/Script/Test_Script/Stm_Scripts/Stage_Mission_Scritps/Stage_Mission_Script.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region 스테이지 미션 스크립트
+/*
+ ▶ 할일
+  - 인게임 매니저를 통해 지정된 미션을 수행
+  - 
+*/
+#endregion
+
+
+public class Stage_Mission_Script : MonoBehaviour
+{
+#region
+
+#endregion
+
+#region
+
+#endregion
+
+    
+}
