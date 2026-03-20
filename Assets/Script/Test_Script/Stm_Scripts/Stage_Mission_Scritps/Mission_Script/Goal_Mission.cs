3402d47c82504c7c042e665703f08e4b2ce581b6
shintaemin <xoalsdl125@gmail.com>
2026-03-20 17:27:45 +0900

    feat , docs : Create MissionScripts , FolderName Chanege
    미션 스크립트 구조 설계 및 생성
    머지를 위한 폴더이름 수정

---
+++ Assets/Script/Test_Script/Stm_Scripts/Stage_Mission_Scritps/Mission_Script/Goal_Mission.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region
+
+#endregion
+
+
+public class Goal_Mission : MonoBehaviour
+{
+#region
+
+#endregion
+
+#region
+
+#endregion
+
+    
+}
