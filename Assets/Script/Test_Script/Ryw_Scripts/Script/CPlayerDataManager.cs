9edb7db96c2dff480676d2db8d07a9775bd151c6
NuahNu <104414203+NuahNu@users.noreply.github.com>
2026-04-01 18:25:13 +0900

    chore : UI를 위한 정보
    부터 생각나는대로 추가중
    
    인게임에서 사용할 데이터를 위한 프로퍼티도 만들어야한다. 노션 참고해서 만든다.

--- Assets/Script/Test_Script/Ryw_Scripts/Script/CPlayerDataManager.cs
+++ Assets/Script/Test_Script/Ryw_Scripts/Script/CPlayerDataManager.cs
@@ -37,12 +37,59 @@ public class CPlayerDataManager : MonoBehaviour, IJsonData
 
     [SerializeField] private int[] _currentUpgradeLevel = new int[3];
     [SerializeField] private int[] _currentTalentLevel = new int[9];
+
+    // 이 부분은 고민이 필요하다.
+    // 딕셔너리로 저장하면 편하겠지만, 직렬화를 위한 추가 처리가 필요하다.
+    // UI를 기준으로 정한다면 그냥 배열로 하면 되지만, UI와의 지속적인 연동이 필요한데... 이러면 책임 역전 아닌가?
+    //[SerializeField] private bool[] _weaponUnLock;
+    //[SerializeField] private bool[] _ClothesUnLock;
     #endregion
 
     #region 내부 변수
+    // 저장 직전 자신의 데이터를 덮어씌우는 부분이 있긴 하지만
+    // 실제 저장 / 불러오기는 이 객체를 기준으로 이루어 진다.
     private PlayerSaveData _data;
     #endregion
+
+    #region 프로퍼티
+    public int Gem => _gem;
+    public int Energy => _energy;
+    public int CurrentWeaponID => _currentWeaponID;
+    public int CurrentClothesID => _currentClothesID;
+
+    public int[] CurrentUpgradeLevel => _currentUpgradeLevel;
+    public int[] CurrentTalentLevel => _currentTalentLevel;
+
+    public int CurrentUpgradeSum
+    {
+        get
+        {
+            int sum = 0;
+
+            for(int i = 0; i< 9; i++)
+            {
+                sum += _currentUpgradeLevel[i];
+            }
+
+            return sum;
+        }
+    }
+    public int CurrentTalentSum
+    {
+        get
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                sum += _currentTalentLevel[i];
+            }
+
+            return sum;
+        }
+    }
     public object SaveData { get => _data; set => _data = (PlayerSaveData)value; }
+    #endregion
 
     void Awake()
     {
@@ -56,14 +103,35 @@ public class CPlayerDataManager : MonoBehaviour, IJsonData
 
     void Update()
     {
-        
+
     }
+
+    public bool TryUseEnergy(int energy)
+    {
+        if (_energy >= energy)
+        {
+            _energy -= energy;
+            return true;
+        }
+        return false;
+    }
+
+    public bool TryUseGem(int gem)
+    {
+        if (_gem >= gem)
+        {
+            _gem -= gem;
+            return true;
+        }
+        return false;
+    }
+
 
     public void MakeSaveData()
     {
         if (_data == null)
             _data = new PlayerSaveData();
-        
+
         _data.Gem = _gem;
         _data.Energy = _energy;
 
@@ -71,7 +139,7 @@ public class CPlayerDataManager : MonoBehaviour, IJsonData
         _data.CurrentClothesID = _currentClothesID;
 
         _data.CurrentUpgradeLevel = _currentUpgradeLevel;
-        _data.CurrentTalentLevel = _currentTalentLevel;   
+        _data.CurrentTalentLevel = _currentTalentLevel;
     }
 
     public void LoadSaveData()
