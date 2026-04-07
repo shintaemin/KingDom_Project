10a430972856b79c6bdc9f403413e3879d71bf87
shintaemin <xoalsdl125@gmail.com>
2026-04-07 19:46:39 +0900

    feat : Create Map_Stage
    맵 스테이지 8, 9 작업
    문 연결 및 스크립트 수정 완료

--- Assets/Script/Test_Script/Stm_Scripts/Stage_Mission_Scritps/Map_Script/Door_Scripts/Door_StageEnd_Col.cs
+++ Assets/Script/Test_Script/Stm_Scripts/Stage_Mission_Scritps/Map_Script/Door_Scripts/Door_StageEnd_Col.cs
@@ -16,10 +16,42 @@ public class Door_StageEnd_Col : MonoBehaviour
 {
     #region 인스펙터
     [SerializeField] private string _colTarget = "Player";
+    [SerializeField] private Transform _endPos;
+    [SerializeField] private Collider _col;
     #endregion
 
     #region 이벤트
     public event Action<Door_StageEnd_Col> OnStageEnd;
+    #endregion
+
+    private void Awake()
+    {
+        if (_col == null)
+        {
+            if (!TryGetComponent<Collider>(out _col))
+            {
+                Debug.Log($"[Door_StageEnd_Col] : 콜라이더 없음");
+                return;
+            }
+        }
+        if (_endPos == null)
+        {
+            _endPos = transform;
+        }
+
+        _col.isTrigger = false;
+    }
+
+    #region 외부 호출 함수
+    public void SetTrigger(bool trigger)
+    {
+        if (_col == null)
+        {
+            return;
+        }
+
+        _col.isTrigger = trigger;
+    }
     #endregion
 
     private void OnTriggerEnter(Collider other)
@@ -29,6 +61,15 @@ public class Door_StageEnd_Col : MonoBehaviour
             return;
         }
 
-        OnStageEnd.Invoke(this);
+        if (other.TryGetComponent<PlayerMover>(out PlayerMover pMover))
+        {
+            //pMover.RoomClearMoveToDoor(_endPos.position);
+
+            float dis = Vector3.Distance(_endPos.position, pMover.transform.position);
+            if (dis <= 0.001f)
+            {
+                OnStageEnd?.Invoke(this);
+            }
+        }
     }
 }
