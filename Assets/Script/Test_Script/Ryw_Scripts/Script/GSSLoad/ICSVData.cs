0c0c704ca18fcce588008ccc609292c5146fcd88
NuahNu <104414203+NuahNu@users.noreply.github.com>
2026-04-08 14:57:32 +0900

    chore : 주석 및 잉여 제거

--- Assets/Script/Test_Script/Ryw_Scripts/Script/GSSLoad/ICSVData.cs
+++ Assets/Script/Test_Script/Ryw_Scripts/Script/GSSLoad/ICSVData.cs
@@ -4,7 +4,10 @@
 /*
 ▶ 작성자 류연우
 
-CGSSLoader에서 읽어온 데이터를 파싱할 수 있는 객체를 위한 인터페이스?
+CGSSLoader에서 읽어온 데이터를 파싱할 수 있는 객체를 위한 인터페이스
+
+    static readonly string NAME = "AbilityData";
+처럼 이름을 저장하는 필드가 꼭 있는데 이거를 여기서 강제한다?
 */
 #endregion
 
