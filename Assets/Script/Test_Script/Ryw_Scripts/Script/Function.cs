b406431ff6c9182318d097f1a1627095f7f1cec3
NuahNu <104414203+NuahNu@users.noreply.github.com>
2026-04-09 15:56:51 +0900

    chore : 그냥 로그가 아니다
    경고다

--- Assets/Script/Test_Script/Ryw_Scripts/Script/Function.cs
+++ Assets/Script/Test_Script/Ryw_Scripts/Script/Function.cs
@@ -22,7 +22,7 @@ public static partial class Function
         {
             if (unityObj == null)
             {
-                Debug.Log($"[Unity Object] {varName} is null or destroyed.");
+                Debug.LogWarning($"[Unity Object] {varName} is null or destroyed.");
                 body?.Invoke();
                 return true;
             }
@@ -31,7 +31,7 @@ public static partial class Function
         {
             if (obj == null)
             {
-                Debug.Log($"{varName} == null");
+                Debug.LogWarning($"{varName} == null");
                 body?.Invoke();
                 return true;
             }
@@ -43,7 +43,7 @@ public static partial class Function
     {
         if (objArr == null || objArr.Count == 0)
         {
-            Debug.Log($"{varName} == null || obj.Length == 0");
+            Debug.LogWarning($"{varName} == null || obj.Length == 0");
             body?.Invoke();
             return true;
         }
