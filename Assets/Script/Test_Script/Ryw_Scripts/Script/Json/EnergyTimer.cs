8ed40e547889e331722216f4653976e56c2c47cc
NuahNu <104414203+NuahNu@users.noreply.github.com>
2026-04-17 16:51:15 +0900

    feat : 에너지 타이머
    MonoBehaviour를 사용하는 방식

---
+++ Assets/Script/Test_Script/Ryw_Scripts/Script/Json/EnergyTimer.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+
+#region EnergyTimer
+/*
+▶ 작성자 류연우
+*/
+#endregion
+
+public class EnergyTimer : MonoBehaviour
+{
+    private static readonly string TIMEKEY = "LastTime";
+    #region 내부 변수
+    public event Action<double> _elapsedTime;
+    private DateTime _lastTime;
+    #endregion
+
+    void Awake()
+    {
+        // 이게 살아날때 시간을 불러온다.
+        string lastTime = PlayerPrefs.GetString(TIMEKEY);
+        if (DateTime.TryParse(lastTime, out _lastTime)) { }
+        else
+        {
+            _lastTime = new DateTime();
+        }
+    }
+
+    private void Start()
+    {
+        Timeelapsed();
+    }
+    private void OnDestroy()
+    {
+        // 여기서 호출하므로 유니티 라이프 사이클에 의해
+        // 다른 구독자들이 메시지를 받지 못한다.
+        // == 시간을 저장만 한다.
+        Timeelapsed();
+    }
+
+    public void Timeelapsed()
+    {
+        DateTime now = DateTime.UtcNow;
+        if (_lastTime != now)
+        {
+            PlayerPrefs.SetString(TIMEKEY, DateTime.UtcNow.ToString());
+
+            // 경과시간
+            TimeSpan elapsed = now - _lastTime;
+            _elapsedTime?.Invoke(elapsed.TotalSeconds);
+
+            // 마지막 시간 업데이트
+            _lastTime = now;
+        }
+    }
+}
